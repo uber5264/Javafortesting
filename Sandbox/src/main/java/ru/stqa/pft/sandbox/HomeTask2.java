235a97d35cefeaab1134236890237d6ccd28c89b
sindzi <uber5264@gmail.com>
2019-11-21 10:02:13 -0500

    Home task 2 was fixed and updated.

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/HomeTask2.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/HomeTask2.java
@@ -4,11 +4,12 @@ public class HomeTask2 {
 	public static void main(String args[]) 	{
     Point p1 = new Point (5,5);
     Point p2 = new Point (10, 10);
-    PointM p3 = new PointM (5,5, 10, 10);
-		System.out.println("Расстояние между точкой P1 ("+p1.x+","+p1.y+") и точкой P2 ("+p2.x+","+p2.y+"), рассчитанное с " +
-						"помощоью функции = "+ distance(p1, p2));
-		System.out.println("Расстояние между точкой P1 ("+p3.x1+","+p3.y1+") и точкой P2 ("+p3.x2+","+p3.y2+"), рассчитанное " +
-						"с помощью метода в класса Point = "+ p3.distance());
+    PointM p3 = new PointM (5,5);
+		PointM p4 = new PointM (10,10);
+		System.out.println("Расстояние между точкой P1 ("+p1.x+","+p1.y+") и точкой P2 ("+p2.x+","+p2.y+"), рассчитанное " +
+						"с " + 	"помощью функции = "+ distance(p1, p2));
+		System.out.println("Расстояние между точкой P1 ("+p3.x1+","+p3.y1+") и точкой P2 ("+p4.x1+","+p4.y1+"), " +
+						"рассчитанное" + " " +	"с помощью метода в класса Point = "+ p3.distance(p4));
 	}
 
 	public static double distance(Point p1, Point p2) {
