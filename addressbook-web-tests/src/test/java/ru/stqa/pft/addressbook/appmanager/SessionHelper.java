5b5280e9377211ccc654212e7cae5bfb09c7850f
sindzi <uber5264@gmail.com>
2019-11-26 12:09:37 -0500

    Fix

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -11,12 +11,8 @@ public class SessionHelper extends HelperBase {
   }
 
   public void login(String username, String password) {
-
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    type(By.name("user"),username);
+    type(By.name("pass"),password);
+    click(By.xpath("//input[@value='Login']"));
   }
 }
