d763f0cdebcecd8c5f419974d97c780a3d9b9d98
sindzi <uber5264@gmail.com>
2019-11-27 12:19:39 -0500

    added GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("testM1", "testN2", "testL3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getNavigationHelper().goToGroupPage();
+
+  }
+
+}
