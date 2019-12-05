e93f6073a197b8db25f5b5358bfdf588082cf515
sindzi <uber5264@gmail.com>
2019-12-05 10:16:41 -0500

    ContactModification verification is added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -9,8 +9,15 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification() {
     app.getNavigationHelper().returnToHomePage();
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("John", "Johnson", "Jojnson",
+              "JJonson", "Mr.", "super Big company",
+              "Russia, St Petersburgh", "123-56-67", "john.johnson@rambler.ru",
+              "www.homepage.ru", "1985", "February", "2", "2", "February",
+              "2015", "test1"), true);
+    app.getNavigationHelper().returnToHomePage(); }
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("John", "Johnson", "Jojnson",
+    app.getContactHelper().fillContactForm(new ContactData("Robert", "Stivenson", "Jojnson",
             "JJonson", "Mr.", "super Big company",
             "Russia, St Petersburgh", "123-56-67", "john.johnson@rambler.ru",
             "www.homepage.ru", "1985", "February", "2", "2", "February",
