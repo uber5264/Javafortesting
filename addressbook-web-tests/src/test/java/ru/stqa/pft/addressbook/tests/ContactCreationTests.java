6d17543f9a5bbb1b1527ca91ca3b968443b4381f
sindzi <uber5264@gmail.com>
2019-12-27 17:06:42 -0500

    lesson 4.9 contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,13 +20,8 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() +1);
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max)  {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 
