3b0930fb118463e1e2be012944f5a351f32989b0
sindzi <uber5264@gmail.com>
2020-01-02 13:39:43 -0500

    fix

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -86,8 +86,7 @@ public class ContactHelper extends HelperBase {
        List<WebElement> cells = element.findElements(By.tagName("td"));
        String lName = cells.get(1).getText();
        String fName = cells.get(2).getText();
-  //   String id = element.findElement(By.tagName("input")).getAttribute("value");
-      int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
       contacts.add(new ContactData(id, fName, lName, null));
     }
     return contacts;
