a7fa4474ac12fe3152c2b9514ec97e43a3a0edde
sindzi <uber5264@gmail.com>
2020-01-16 17:50:48 -0500

    lesson 6.11 fixed

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,15 +6,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+
 public class TestBase {
 
- // Logger logger = LoggerFactory.getLogger(TestBase.class);
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
@@ -30,13 +30,13 @@ public class TestBase {
   }
    @BeforeMethod
   public void logTestStart(Method m, Object[] p) {
-  //   logger.info("Start test " + m.getName() + "with parameters" + Arrays.asList(p));
+  logger.info("Start test " + m.getName() + "with parameters" + Arrays.asList(p));
 
   }
 
   @AfterMethod (alwaysRun = true)
   public void logTestStop(Method m) {
-    //logger.info("Stop test " + m.getName());
+  logger.info("Stop test " + m.getName());
 
   }
 }
