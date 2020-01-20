a50d76ebacf9686ecda1f8d56ae562c7e00bdde5
sindzi <uber5264@gmail.com>
2020-01-19 20:39:19 -0500

    lesson 7.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -40,6 +40,7 @@ public class ContactCreationTests extends TestBase {
     assertThat(after.size(), equalTo(before.size() + 1));
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    verifyContactListInUI();
   }
   @Test (enabled = false )
   public void testContactCreation() throws Exception {
